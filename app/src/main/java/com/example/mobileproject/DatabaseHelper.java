fc14f9472297ae7fedb72f60b7223679723674af
Nour Tuffaha <nouraldeen123@outlook.com>
2022-12-23 16:03:49 +0200

    ,

--- app/src/main/java/com/example/mobileproject/DatabaseHelper.java
+++ app/src/main/java/com/example/mobileproject/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.mobileproject;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -16,6 +18,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_BRAND = "Brand";
     private static final String COLUMN_MODEL = "Model";
     private static final String COLUMN_GENDER = "Gender";
+    private static final Double COLUMN_COST = Double.valueOf(0);
     private static final String COLUMN_ASPECT1 = "Aspect1";
     private static final String COLUMN_ASPECT2 = "Aspect2";
     private static final String COLUMN_ASPECT3 = "Aspect3";
@@ -37,6 +40,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                         COLUMN_BRAND + " TEXT," +
                         COLUMN_MODEL + " TEXT," +
                         COLUMN_GENDER + " TEXT," +
+                        COLUMN_COST + " TEXT," +
                         COLUMN_ASPECT1 + " TEXT," +
                         COLUMN_ASPECT2 + " TEXT," +
                         COLUMN_ASPECT3 + " TEXT);";
@@ -49,5 +53,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+
+    void addData(String watchID, String Brand, String Model, String Gender, Double Cost, String Aspect1, String Aspect2, String Aspect3){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+                cv.put(COLUMN_ID, watchID);
+                cv.put(COLUMN_BRAND, Brand);
+                cv.put(COLUMN_MODEL, Model);
+                cv.put(COLUMN_GENDER, Gender);
+                cv.put(String.valueOf(COLUMN_COST), Cost);
+                cv.put(COLUMN_ASPECT1, Aspect1);
+                cv.put(COLUMN_ASPECT2, Aspect2);
+                cv.put(COLUMN_ASPECT3, Aspect3);
+
+                long result = db.insert(TABLE_NAME,null,cv);
+                if (result == -1)
+                    Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(context, "Successfully Added!", Toast.LENGTH_SHORT).show();
+
+
     }
 }
--- app/src/main/java/com/example/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/MainActivity.java
@@ -20,6 +20,28 @@ public class MainActivity extends AppCompatActivity {
         Button menBtn = findViewById(R.id.menBtn);
         Button womenBtn = findViewById(R.id.womenBtn);
 
+        Watch[] watchArray = new Watch[10];
+
+
+        try {
+            FileManager.read();
+            watchArray = FileManager.watchArray;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        DatabaseHelper myDB = new DatabaseHelper(MainActivity.this);
+        for (int i = 0; i < watchArray.length; i++) {
+            myDB.addData(watchArray[i].getWatch_id(),
+                    watchArray[i].getBrand(),
+                    watchArray[i].getModel(),
+                    watchArray[i].getGender(),
+                    watchArray[i].getCost(),
+                    watchArray[i].getAspect1(),
+                    watchArray[i].getAspect2(),
+                    watchArray[i].getAspect3()
+            );
+
+        }
         allBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
